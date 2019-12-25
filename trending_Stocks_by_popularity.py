e96498035eade8fbc62c9d3ada37aac27750a3ef
Sarvesh Dubey <dubeysarvesh5525@gmail.com>
2019-12-25 17:22:13 +0530

    Create trending_Stocks_by_popularity.py

---
+++ trending_Stocks_by_popularity.py
@@ -0,0 +1,31 @@
+from bs4 import BeautifulSoup
+import requests
+from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
+import time
+import random
+
+while True:
+    try:
+        driver = webdriver.Chrome(ChromeDriverManager().install())
+        driver.get('https://www.investing.com/equities/trending-stocks')
+
+        res = driver.execute_script("return document.documentElement.outerHTML")
+
+        driver.quit()
+
+        soup = BeautifulSoup(res,'lxml')
+        soup = soup.find('div',attrs = {'class':'trendingStocks'})
+        soup = soup.find('div',attrs = {'class' : 'chartContainer'})
+        soup = soup.find('div',attrs = {'id':'trendingByPopularityChart','class':'dirLtr trendingStocksChart'})
+        soup = soup.find('div',attrs = {'id':'highcharts-0','class':'highcharts-container'})
+        soup = soup.find('svg',attrs = {'style':'font-family:"Lucida Grande", "Lucida Sans Unicode", Arial, Helvetica, sans-serif;font-size:12px;'})
+        soup = soup.find('g',attrs = {'class':'highcharts-axis-labels highcharts-xaxis-labels'})
+
+        company = []
+        for rows in soup.findAll('text'):
+            company.append(rows.text)
+        print(company)
+    except:
+        print('Let me sleep for some time')
+        time.sleep(random.uniform(0.2,5))
